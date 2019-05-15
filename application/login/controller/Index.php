8538f2566b94bc0420f370aea4efdd23e217800a
朱冠宇 <2043193520@qq.com>
2019-05-15 23:01:35 +0800

    CheackForRegister

--- application/login/controller/Index.php
+++ application/login/controller/Index.php
@@ -11,15 +11,23 @@ use think\Controller;
 class Index extends Controller
 {//anumber:账号    password:密码     pnumber:手机号码    email:邮箱    vcode:验证码
 
+    public $truecode="hello";
+
     public function login()
     {
         return view("login");
     }
 
-    public function register($data=["anumber"=>"","password"=>"","passwordcopy"=>""," pnumber"=>"","email"=>"","vcode"=>""],$waring=["anb_war"=>"*必填","ps_war"=>"*必填","psc_war"=>"*必填","pn_war"=>"*必填","em_war"=>"*必填","vc_war"=>"*必填"])
+//    public function register($data=["anumber"=>"","password"=>"","passwordcopy"=>"","pnumber"=>"","email"=>"","vcode"=>""],$waring=["anb_war"=>"*必填","ps_war"=>"*必填","psc_war"=>"*必填","pn_war"=>"*必填","em_war"=>"*必填","vc_war"=>"*必填"])
+//    {
+//        $this->assign($data);
+//        $this->assign($waring);
+//        return view("register");
+//    }
+
+    public function register()
     {
-        $this->assign($data);
-        $this->assign($waring);
+        $this->assign(["truecode"=> "hello"]);
         return view("register");
     }
 
@@ -30,25 +38,35 @@ class Index extends Controller
     //对注册用户进行检测
     public function registercheck(Request $request)
     {
-        $i=0;
-        $war=["anb_war","ps_war","psc_war","pn_war","em_war","vc_war"];
-        $data=$request->param();//anumber:账号    password:密码     passwordcopy:重复密码     pnumber:手机号码    email:邮箱    vcode:验证码
-        $warning=["anb_war"=>"","ps_war"=>"","psc_war"=>"","pn_war"=>"","em_war"=>"","vc_war"=>""];//anb_war:账号提示错误      ps_war:密码提示错误      pn_war:手机号码提示错误      em_war:邮箱提示错误
-        $sign=true;
+        $ifnull=true;
+//        $i=0;
+//        $war=["anb_war","ps_war","psc_war","pn_war","em_war","vc_war"];
+        $data=$request->param();//username:用户名    password:密码     passwordcopy:重复密码     phonenumber:手机号码    email:邮箱    code:验证码
+//        $warning=["anb_war"=>"","ps_war"=>"","psc_war"=>"","pn_war"=>"","em_war"=>"","vc_war"=>""];//anb_war:账号提示错误      ps_war:密码提示错误      pn_war:手机号码提示错误      em_war:邮箱提示错误
 
         //循环查找是否为空
         foreach ($data as $key=>$value){
             if($value=="") {
-                $warning[$war[$i]] = config($key)."不能为空！";
-                $sign=false;
+                $ifnull=false;
+//                $warning[$war[$i]] = config($key)."不能为空！";
+//                $sign=false;
             }
-            $i++;
+//            $i++;
         }
 
-        if(!$sign){
-            return $this->register($data,$warning);
+        dump($data);
+
+        if(!$ifnull){
+            $this->error('必填项不能为空！');
         }
-        else{
-            return $this->login();       }
+        else if($data["password"]!=$data["passwordcopy"]){
+            $this->error('两次填写密码不相同！');
+        }
+        else if($data["code"]!="hello"){
+            $this->error('验证码不正确');
+        }
+        else {
+            $this->success('注册成功！','http://localhost/signin');
+        }
     }
 }
